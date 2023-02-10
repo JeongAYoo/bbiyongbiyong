d48435f4c5f436bff36d6d5146de2c0783a89950
JeongAYoo <jeonga038@naver.com>
2023-02-11 05:20:48 +0900

    Initial Commit

---
+++ Bbiyong-Biyong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bbiyong-Biyong
+//
+//  Created by Jade Yoo on 2023/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
